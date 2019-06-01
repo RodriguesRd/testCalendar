4cae318cda6b6941b7d399eadeb6607dbd5910eb
Rodrigo Rodrigues <rodrigues.rd@gmail.com>
2019-06-01 15:46:39 +0100

    2 commit

---
+++ TestCalendar/Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCEventCalendar.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventID { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime end { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsFullDay { get; set; }
+
+
+
+    }
+}
